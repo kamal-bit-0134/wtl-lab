4819409c7b225f22c535cf9719ae9526e8806909
Kamal Yogi <69454533+kamal-bit-0134@users.noreply.github.com>
2022-05-18 12:38:59 +0530

    Add files via upload

---
+++ demo.php
@@ -0,0 +1,16 @@
+<html>
+    <body>
+       <?php
+        echo "HELLO WORLD";
+        echo "<h2>BOOM</h2>";
+        print "print";
+        // variabes
+        $cookies=23;
+        $price = 3;
+        $total = $cookies * $price;
+        echo "<h1 style=color:red >$total</h1>";
+       ?>        
+        
+    </body>
+
+</html>
